2e7f2e6319118db377fa0c08a9b37edc2759a717
BugaCo <li_yizhe@icloud.com>
2021-05-18 19:52:03 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by BugaCo on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
